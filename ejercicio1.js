237ff6a5a8f73a17c01b92dc7f86302881ea3e84
vicky <mdep665@g.educaand.es>
2025-10-24 14:41:37 +0200

    añado ejercicio1

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,3 +4,16 @@
 
 let prompt = require('prompt-sync')();
 
+let a = prompt('Introduzca a:');
+let b = prompt('Introduzca b:');
+
+console.log(`El valor original de a es: ${a}`);
+console.log(`El valor original de b es: ${b}`);
+
+let b2 = b;
+
+b = a;
+a = b2;
+
+console.log(`El nuevo valor de a es: ${a}`);
+console.log(`El nuevo valor de b es: ${b}`);
